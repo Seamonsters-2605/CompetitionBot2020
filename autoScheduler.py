a9a1f70d24c9bad21f89e06b0f9aac49288b7bb2
Trent1048 <trentbultsma@gmail.com>
2019-10-31 17:53:19 -0700

    add AutoScheduler from last year

---
+++ autoScheduler.py
@@ -0,0 +1,25 @@
+class AutoScheduler:
+
+    def __init__(self):
+        self.actionList = []
+        self.runningAction = None
+        self.updateCallback = lambda: None
+        self.idleFunction = lambda: None
+
+    def runSchedule(self):
+        try:
+            while True:
+                if len(self.actionList) == 0:
+                    self.runningAction = None
+                    self.updateCallback()
+                    while len(self.actionList) == 0:
+                        self.idleFunction()
+                        yield
+                        continue
+                self.runningAction = self.actionList[0]
+                self.updateCallback()
+                yield from self.runningAction.function()
+                if self.runningAction in self.actionList:
+                    self.actionList.remove(self.runningAction)
+        finally:
+            self.runningAction = None
