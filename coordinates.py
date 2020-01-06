b57d91f6064cd93734c11a0c65f91acd4158b294
Trent1048 <trentbultsma@gmail.com>
2020-01-06 15:56:20 -0800

    add FieldCoordinates to store values for locations on the field

---
+++ coordinates.py
@@ -0,0 +1,13 @@
+import math
+
+class FieldCoordinate:
+
+    def __init__(self, name, x, y, angle):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.angle = angle
+
+    def __repr__(self):
+        return "%s (%f, %f, %f deg)" \
+            % (self.name, self.x, self.y, math.degrees(self.angle))
