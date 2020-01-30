7b0d69d05820964b87f6397809958bd3be8badeb
Trent1048 <trentbultsma@gmail.com>
2020-01-29 17:20:23 -0800

    add important field coordinates

--- coordinates.py
+++ coordinates.py
@@ -1,5 +1,7 @@
 import math
 
+# field: 52, 26
+
 class FieldCoordinate:
 
     def __init__(self, name, x, y, angle):
@@ -12,4 +14,13 @@ class FieldCoordinate:
         return "%s (%f, %f, %f deg)" \
             % (self.name, self.x, self.y, math.degrees(self.angle))
 
-targetPoints = []
+targetPoints = [
+    # Red
+    FieldCoordinate("Low Port", -24.5, 5.11, math.pi/2),
+    FieldCoordinate("Control Panel", 0, 10.69, 3 * math.pi/2),
+    FieldCoordinate("Loading Bay", 24.5, 4.625, 3 * math.pi/2),
+    # Blue
+    FieldCoordinate("Low Port", 24.5, -5.11, 3 * math.pi/2),
+    FieldCoordinate("Control Panel", 0, -10.69, math.pi/2),
+    FieldCoordinate("Loading Bay", -24.5, -4.625, math.pi/2)
+]
