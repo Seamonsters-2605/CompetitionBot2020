5e15aef1f0aca5069d3f4eb9094581a8f7acd3ee
Trent1048 <trentbultsma@gmail.com>
2020-02-04 16:09:07 -0800

    take everything away except the dashbord

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import remi.gui as gui
 import math
 import seamonsters as sea
 
-class CompetitionDashboard(sea.Dashboard):
+class RevDashboard(sea.Dashboard):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, css=True, **kwargs)
@@ -18,117 +18,16 @@ class CompetitionDashboard(sea.Dashboard):
     def main(self, robot, appCallback):
         self.robot = robot
 
-        root = gui.HBox(width = 1000, margin = "0px auto")
+        root = gui.VBox(width = 300, margin = "0px auto")
         root.style['align-items'] = 'stretch'
 
-        leftSide = gui.VBox()
-        leftSide.style['align-items'] = 'stretch'
-
-        middle = gui.VBox()
-
-        rightSide = gui.VBox()
-        rightSide.style['align-items'] = 'flex-start'
-
-        self.motorDataDict = { \
-            "ampRow" : None,
-            "tempRow" : None,
-            "maxAmpRow" : None,
-            "maxTempRow" : None,
-            "amp" : [],
-            "temp" : [],
-            "maxAmp" : [],
-            "maxTemp" : []
-            }
-
-        leftSide.append(self.initStats(robot))
-        leftSide.append(self.initLedControl(robot))
-
-        middle.append(self.initCamera(robot))
-
-        rightSide.append(self.initManual(robot))
-        rightSide.append(self.initTest(robot, "Motor Test 1"))
-        rightSide.append(self.initTest(robot, "Motor Test 2"))
-
-        root.append(leftSide)
-        root.append(middle)
-        root.append(rightSide)
+        root.append(self.initTest(robot, 1))
+        root.append(self.initTest(robot, 2))
 
         appCallback(self)
         return root
 
-    # runs every time the dashboard is updated
-    def idle(self):
-
-        # updates the values in self.motorDataTable
-        for motorNum in range(6):
-
-            ampRow = self.motorDataTable.children[self.motorDataDict["ampRow"]]
-            ampItem = ampRow.children[self.motorDataDict["amp"][motorNum]]
-            ampItem.set_text(str(self.robot.motorData[motorNum]["amps"]))
-
-            tempRow = self.motorDataTable.children[self.motorDataDict["tempRow"]]
-            tempItem = tempRow.children[self.motorDataDict["temp"][motorNum]]
-            tempItem.set_text(str(self.robot.motorData[motorNum]["temp"]))
-
-            maxAmpRow = self.motorDataTable.children[self.motorDataDict["maxAmpRow"]]
-            maxAmpItem = maxAmpRow.children[self.motorDataDict["maxAmp"][motorNum]]
-            maxAmpItem.set_text(str(self.robot.motorData[motorNum]["maxAmp"]))
-
-            maxTempRow = self.motorDataTable.children[self.motorDataDict["maxTempRow"]]
-            maxTempItem = maxTempRow.children[self.motorDataDict["maxTemp"][motorNum]]
-            maxTempItem.set_text(str(self.robot.motorData[motorNum]["maxTemp"]))
-
-    def initManual(self, robot):
-        manualBox = self.sectionBox()
-
-        driveControlBox = gui.VBox(gui.Label("Drive controls"))
-        gearButtons = []
-        speedButtons = []
-        compressorButtons = []
-        
-        self.gearGroup = sea.ToggleButtonGroup()
-        for mode in robot.driveGears.keys():
-            button = gui.Button(mode)
-            button.set_on_click_listener(robot.c_changeGear)
-            gearButtons.append(button)
-            self.gearGroup.addButton(button)
-        
-        self.speedGroup = sea.ToggleButtonGroup()
-        for speed in robot.driveGears[robot.driveMode].keys():
-            button = gui.Button(speed)
-            button.set_on_click_listener(robot.c_changeSpeed)
-            speedButtons.append(button)
-            self.speedGroup.addButton(button)
-
-        self.compressorGroup = sea.ToggleButtonGroup()
-        for mode in ["start","stop"]:
-            button = gui.Button(mode)
-            button.set_on_click_listener(robot.c_compressor)
-            compressorButtons.append(button)
-            self.compressorGroup.addButton(button)
-        
-        gearBox = sea.hBoxWith(gui.Label("Gears:"), gearButtons)
-        speedBox = sea.hBoxWith(gui.Label("Speed:"), speedButtons)
-        compressorBox = sea.hBoxWith(gui.Label("Compressor:"), compressorButtons)
-
-        driveControlBox.append(gearBox)
-        driveControlBox.append(speedBox)
-        driveControlBox.append(compressorBox)
-
-        manualBox.append(driveControlBox)
-        return manualBox
-
-    def initCamera(self, robot):
-        cameraBox = self.sectionBox()
-
-        videoFeedBox = gui.HBox()
-        videoFeed = gui.Image('http://10.26.5.6:5800/')
-        videoFeedBox.append(videoFeed)
-
-        cameraBox.append(videoFeedBox)
-        return cameraBox
-
-    def initTest(self, robot, label):
+    def initTest(self, robot, testNum):
         testBox = self.sectionBox()
 
         motorNumberIn = gui.Input()
@@ -139,84 +38,29 @@ class CompetitionDashboard(sea.Dashboard):
         motorSpeedBox = sea.hBoxWith(testButton, gui.Label("Speed:"), motorSpeedInput)
 
         def testMotor(button):
-            robot.superDrive.disable()
             try:
-                motorNum = int(motorNumberIn.get_value()) - 1
+                motorNum = int(motorNumberIn.get_value())
             except:
                 print("Motor number incorrect")
                 return
 
-            wheelNum = 0
-            if motorNum >= 3:
-                wheelNum = 1
-                motorNum -= 3
-
-            if motorNum < 0 or motorNum >= 3:
+            if not 1 <= motorNum <= 9:
                 print("Motor number incorrect")
                 return
         
             testSpeed = float(motorSpeedInput.get_value())
 
-            if testSpeed > -1.0 and testSpeed < 1.0:
-                testSpeed = 0.0
-            else:
-                print("Motor Speed Error")
+            if testSpeed < -1:
+                testSpeed = -1
+            elif testSpeed > 1:
+                testSpeed = 1
 
-            robot.testSettings["wheelNum"] = wheelNum
-            robot.testSettings["motorNum"] = motorNum
-            robot.testSettings["speed"] = testSpeed
+            robot.testSettings[testNum - 1]["motorNum"] = motorNum
+            robot.testSettings[testNum - 1]["speed"] = testSpeed
 
         testButton.set_on_click_listener(testMotor)
 
-        testBox.append(gui.Label(label))
+        testBox.append(gui.Label("Motor Test " + str(testNum)))
         testBox.append(motorSelectionBox)
         testBox.append(motorSpeedBox)
         return testBox
-
-    def initLedControl(self, robot):
-        ledBox = self.sectionBox()
-
-        ledInputBox = sea.hBoxWith(gui.Label("LED Control"))
-        ledSet = gui.Button("Set")
-        ledIn = gui.Input()
-
-        def ledSetValue(button):
-            robot.ledInput = float(ledIn.get_value())
-
-        ledSet.set_on_click_listener(ledSetValue)
-
-        ledInputBox.append(ledSet)
-        ledInputBox.append(ledIn)
-
-        ledBox.append(ledInputBox)
-
-        return ledBox
-
-    def initStats(self, robot):
-        statsBox = self.sectionBox()
-
-        motorDataBox = sea.vBoxWith(gui.Label("Motor Data"))
-        self.motorDataTable = gui.Table()
-        motorNumRow = gui.TableRow(gui.TableItem("Motor Number:"))
-        motorAmpRow = gui.TableRow(gui.TableItem("Amp Draw:"))
-        motorTempRow = gui.TableRow(gui.TableItem("Temp:"))
-        motorMaxAmpRow = gui.TableRow(gui.TableItem("Max Amp Draw:"))
-        motorMaxTempRow = gui.TableRow(gui.TableItem("Max Temp:"))
-
-        self.motorDataTable.append(motorNumRow)
-        self.motorDataDict["ampRow"] = self.motorDataTable.append(motorAmpRow)
-        self.motorDataDict["tempRow"] = self.motorDataTable.append(motorTempRow)
-        self.motorDataDict["maxAmpRow"] = self.motorDataTable.append(motorMaxAmpRow)
-        self.motorDataDict["maxTempRow"] = self.motorDataTable.append(motorMaxTempRow)
-
-        for motorNum in range(6):
-            motorNumRow.append(gui.TableItem(str(motorNum + 1)))
-            self.motorDataDict["amp"].append(motorAmpRow.append(gui.TableItem("")))
-            self.motorDataDict["temp"].append(motorTempRow.append(gui.TableItem("")))
-            self.motorDataDict["maxAmp"].append(motorMaxAmpRow.append(gui.TableItem("")))
-            self.motorDataDict["maxTemp"].append(motorMaxTempRow.append(gui.TableItem("")))
-        
-        motorDataBox.append(self.motorDataTable)
-
-        statsBox.append(motorDataBox)
-        return statsBox
--- robot.py
+++ robot.py
@@ -1,329 +1,73 @@
 import wpilib
 import rev
-import drivetrain
 import seamonsters as sea 
 import math
-import navx
 import dashboard
-import autoScheduler
-import vision
-from networktables import NetworkTables
 
-SOLENOID_FORWARD = wpilib.DoubleSolenoid.Value.kForward
-SOLENOID_REVERSE = wpilib.DoubleSolenoid.Value.kReverse
-
-CONTROLLER_RIGHT = wpilib.interfaces._interfaces.GenericHID.Hand.kRightHand
-CONTROLLER_LEFT = wpilib.interfaces._interfaces.GenericHID.Hand.kLeftHand
-
-class CompetitionBot2020(sea.GeneratorBot):
+class RevClientBot(sea.GeneratorBot):
 
     def robotInit(self):
 
-        # devices
-        self.controller = wpilib.XboxController(0)
-
-        ahrs = navx.AHRS.create_spi()
-
         self.pdp = wpilib.PowerDistributionPanel(50)
 
-        self.ledStrip = wpilib.PWM(0)
-        self.ledInput = -0.99
+        motor1 = rev.CANSparkMax(1, rev.CANSparkMax.MotorType.kBrushless)
+        motor2 = rev.CANSparkMax(2, rev.CANSparkMax.MotorType.kBrushless)
+        motor3 = rev.CANSparkMax(3, rev.CANSparkMax.MotorType.kBrushless)
+        motor4 = rev.CANSparkMax(4, rev.CANSparkMax.MotorType.kBrushless)
+        motor5 = rev.CANSparkMax(5, rev.CANSparkMax.MotorType.kBrushless)
+        motor6 = rev.CANSparkMax(6, rev.CANSparkMax.MotorType.kBrushless)
+        motor7 = rev.CANSparkMax(7, rev.CANSparkMax.MotorType.kBrushless)
+        motor8 = rev.CANSparkMax(8, rev.CANSparkMax.MotorType.kBrushless)
+        motor9 = rev.CANSparkMax(9, rev.CANSparkMax.MotorType.kBrushless)
 
-        self.superDrive = drivetrain.initDrivetrain()
-        # multiDrive allows the robot to be driven multiple times in a loop and the values are averaged
-        self.multiDrive = sea.MultiDrive(self.superDrive) 
-        self.pathFollower = sea.PathFollower(self.superDrive, ahrs)
-
-        self.limelight = NetworkTables.getTable('limelight')
-        self.limelight.putNumber('pipeline', vision.DUAL_PIPELINE)
-
-        # for autonomous mode
-        self.autoScheduler = autoScheduler.AutoScheduler()
-        self.autoScheduler.idleFunction = self.autoIdle
-        self.autoScheduler.updateCallback = self.updateScheduler
+        self.motors = [motor1, motor2, motor3, motor4, motor5, motor6, motor7, motor8, motor9]
 
         # controls the state of the robot
         self.controlModeMachine = sea.StateMachine()
-        self.manualState = sea.State(self.driving)
-        self.autoState = sea.State(self.autoScheduler.runSchedule)
         self.testState = sea.State(self.testing)
 
-        # for shifting gear box
-        self.compressor = wpilib.Compressor(0)
-        self.compressor.stop()
-        self.piston1 = wpilib.DoubleSolenoid(0, 1)
-        self.piston2 = wpilib.DoubleSolenoid(2, 3)
-
-        # drive gears
-        self.superDrive.gear = None
-        self.driveGear = drivetrain.mediumVelocityGear
-        self.driveMode = "velocity"
-        self.driveSpeed = "medium"
-        self.driveGears = \
-            {"voltage" : \
-                {"slow" : drivetrain.slowVoltageGear, 
-                "medium" : drivetrain.mediumVoltageGear, 
-                "fast" : drivetrain.fastVoltageGear}, 
-            "velocity" : \
-                {"slow" : drivetrain.slowVelocityGear, 
-                "medium" : drivetrain.mediumVelocityGear, 
-                "fast" : drivetrain.fastVelocityGear},      
-            "position" : \
-                {"slow" : drivetrain.slowPositionGear, 
-                "medium" : drivetrain.mediumPositionGear, 
-                "fast" : drivetrain.fastPositionGear} 
-            }
-
         # testing
-        self.testSettings = { \
-            "wheelNum" : 0,
-            "motorNum" : 0,
+        self.testSettings = [{ \
+            "motorNum" : 1,
             "speed" : 0
-            }
-
-        # for dashboard motor data
-        self.motorData = [dict() for _ in range(len(self.superDrive.motors))]
-
-        # sets initial values so the dashboard doesn't break when it tries to get 
-        # them before the values are updated in self.updateMotorData
-        for motor in range(len(self.superDrive.motors)):
-            initAmps = round(self.superDrive.motors[motor].getOutputCurrent(), 2) 
-            initTemp = round(self.superDrive.motors[motor].getMotorTemperature(), 2)
-
-            self.motorData[motor]["amps"] = initAmps
-            self.motorData[motor]["temp"] = initTemp
-
-            self.motorData[motor]["maxAmp"] = initAmps
-            self.motorData[motor]["maxTemp"] = initTemp
+            }, { \
+            "motorNum" : 2,
+            "speed" : 0
+            }]
 
         self.app = None 
-        sea.startDashboard(self, dashboard.CompetitionDashboard)
+        sea.startDashboard(self, dashboard.RevDashboard)
 
     # Different Driving Modes
 
     # robot is controlled by the driver
     def teleop(self):
-        self.manualMode()
+        pass
         yield from self.mainGenerator()
 
     # robot is controlled by the code
     def autonomous(self):
-        self.autoMode()
+        pass
         yield from self.mainGenerator()
 
     # robot is being tested off the field
     def test(self):
-        self.testMode()
+        self.controlModeMachine.replace(self.testState)
         yield from self.mainGenerator()
 
     # runs every 50th of a second
     def mainGenerator(self):
-
-        self.superDrive.resetWheelPositions()
-
         yield from sea.parallel(
             self.controlModeMachine.updateGenerator(), 
             self.updateDashboardGenerator(),
             self.updateMotorData())
 
-    # switches the robot into teleop
-    def manualMode(self):
-        for wheel in self.superDrive.wheels:
-            wheel.setIdleMode(rev.CANSparkMax.IdleMode.kBrake)
-         
-        self.piston1.set(SOLENOID_FORWARD)
-        self.piston2.set(SOLENOID_FORWARD)
-
-        self.controlModeMachine.replace(self.manualState)
-
-    # switches the robot into auto
-    def autoMode(self):
-        for wheel in self.superDrive.wheels:
-            wheel.setIdleMode(rev.CANSparkMax.IdleMode.kBrake)
-         
-        self.controlModeMachine.replace(self.autoState)
-
-    # switches the robot into test mode
-    def testMode(self):
-        for wheel in self.superDrive.wheels:
-            wheel.setIdleMode(rev.CANSparkMax.IdleMode.kCoast)
-            
-        self.superDrive.disable()
-        self.controlModeMachine.replace(self.testState)
-
-    # is run in teleop to get input and make the robot go 
-    def driving(self):
-        while True:
-
-            self.pathFollower.updateRobotPosition()
-            
-            if self.superDrive.gear != self.driveGear:
-                self.driveGear.applyGear(self.superDrive)
-                if self.app is not None:
-                    self.app.gearGroup.highlight(self.driveMode)
-                    self.app.speedGroup.highlight(self.driveSpeed)
-
-            if self.piston1.get() == SOLENOID_REVERSE and self.driveSpeed != "slow":
-                self.piston1.set(SOLENOID_FORWARD)
-                self.piston2.set(SOLENOID_FORWARD)
-            elif self.piston1.get() == SOLENOID_FORWARD and self.driveSpeed == "slow":
-                self.piston1.set(SOLENOID_REVERSE)
-                self.piston2.set(SOLENOID_REVERSE)
-
-            turn = sea.deadZone(self.controller.getX(CONTROLLER_RIGHT), deadZone=0.05)
-            turn *= self.driveGear.turnScale
-            mag = -sea.deadZone(self.controller.getY(CONTROLLER_LEFT), deadZone=0.05)
-            mag *= self.driveGear.moveScale
-
-            self.multiDrive.drive(mag, math.pi/2, turn)
-            self.multiDrive.update()
-
-            self.ledStrip.setSpeed(self.ledInput)
-
-            if self.controller.getBumper(1):
-                # the robot works towards aligning with a vision 
-                # target while the bumper is being held down
-                self._turnDegree(None, accuracy=0, multiplier=(20 / self.driveGear.turnScale), visionTarget=True)
-        
-            yield
-
     # switches to use the dashboard for testing purposes
     def testing(self):
         while True:
 
-            self.superDrive.wheels[self.testSettings["wheelNum"]]._drive( \
-                self.testSettings["speed"] * self.driveGear.moveScale, \
-                math.pi/2, self.testSettings["motorNum"])
-
-            yield
-
-    # runs in autonomous when nothing else is going
-    def autoIdle(self):
-        self.pathFollower.updateRobotPosition()
-        self.superDrive.drive(0, 0, 0)
-
-    # Helpful Movement Functions
-
-    # turns the robot a small fraction of the inputted value degree.
-    # if called in a loop, the result will be the robot turning the desired amount
-    def _turnDegree(self, degrees, accuracy=3, multiplier=1, visionTarget=False):
-        # if degrees is None, the robot will *only* try to allign
-        # with a vision target and not move otherwise
-        
-        # visionTarget: weather or not the robot will try to align to a vision target
-
-        if degrees is None and not visionTarget:
-            raise ValueError("visionTarget must be True if degrees is None")
-
-        self.pathFollower.updateRobotPosition()
-
-        if visionTarget:
-
-            if vision.targetDetected(self.limelight):
-                hOffset = -vision.getXOffset(self.limelight)
-
-            elif degrees is None:
-                return False
-
-            # prevents robot from spinning uncontrollably
-            if abs(hOffset) < 180:
-                offset = hOffset
-
-        else:
-            offset = math.degrees(self.pathFollower.robotAngle) + degrees
-
-        # as the robot gets closer to the target angle, it will slow down
-        speed = (-offset / 360) * multiplier
-        if speed > 1:
-            speed = 1
-        speed *= self.driveGear.turnScale
-
-        self.multiDrive.drive(0, math.pi/2, speed) 
-    
-        # returns if the robot is within the desired accuracy
-        return -accuracy < abs(offset) < accuracy
-
-    # turns the robot a certain amount
-    # or aligns with a vision target if visionTarget is True
-    def turnDegrees(self, degrees, accuracy=3, multiplier=1, visionTarget=False):
-
-        # the function ends if the robot is looking for a vision target but there are none
-        if visionTarget:
-            self.limelight.putNumber('pipeline', vision.DUAL_PIPELINE)
-
-            if not vision.targetDetected(self.limelight):
-                return False
-
-        # stops the robot from spinnig uncontrollably
-        if degrees > 180:
-            return False
-
-        accuracy = abs(accuracy)
-        accuracyCount = 0 # the amount of iterations the robot has been within the accuracy range
-
-        while True:
-            accurate = self._turnDegree(degrees, accuracy, multiplier, visionTarget)
-            self.multiDrive.update()
-            
-            if accurate:
-                accuracyCount += 1
-            else:
-                accuracyCount = 0
-
-            if accuracyCount > 5:
-                return True
-
-            yield
-
-    def faceVisionTarget(self):
-        self.limelight.putNumber('pipeline', vision.DUAL_PIPELINE)
-        hOffset = vision.getXOffset(self.limelight)
-
-        if not vision.targetDetected(self.limelight) or abs(hOffset) > 180:
-            return False
-
-        yield from self.turnDegrees(-hOffset, accuracy=2, multiplier=9, visionTarget=True)
-
-    # drives the robot a specified distance in a straight line
-    # distance measured in feet
-    def driveDist(self, distance, accuracy=0.025, slowDist=1):
-        accuracy = abs(accuracy)
-        accuracyCount = 0 # the amount of iterations the robot has been within the accuracy range
-
-        # the right side is negative because the motor is turned around
-        targetDistL = self.superDrive.wheels[0].getRealPosition() + distance
-        targetDistR = -self.superDrive.wheels[1].getRealPosition() + distance
-        targetDist = (targetDistL + targetDistR) / 2
-
-        def getLocation():
-            locationL = self.superDrive.wheels[0].getRealPosition()
-            locationR = -self.superDrive.wheels[1].getRealPosition()
-            location = (locationL + locationR) / 2
-
-            return location
-
-        while True:
-            self.pathFollower.updateRobotPosition()
-
-            offset = targetDist - getLocation()
-            
-            # the robot will start to slow down once it is 
-            # slowDist feet away from the target
-            speed = offset / slowDist 
-            if speed > 1:
-                speed = 1
-            speed *= self.driveGear.moveScale
-            
-            self.multiDrive.drive(speed, math.pi/2, 0)
-            self.multiDrive.update()
-
-            if -accuracy < abs(offset) < accuracy:
-                accuracyCount += 1
-            else:
-                accuracyCount = 0
-
-            if accuracyCount > 5:
-                return True
+            self.motors[self.testSettings[0]["motorNum"]].set(self.testSettings[0]["speed"])
+            self.motors[self.testSettings[1]["motorNum"]].set(self.testSettings[1]["speed"])
 
             yield
 
@@ -356,37 +100,5 @@ class CompetitionBot2020(sea.GeneratorBot):
 
             yield
 
-    # updates self.autoScheduler to run functions in autonomous
-    def updateScheduler(self):
-        # needs work
-
-        pass
-
-    # Dashboard Callbacks
-    
-    @sea.queuedDashboardEvent
-    def c_changeGear(self, button):
-        self.driveMode = button.get_text()
-        self.driveGear = self.driveGears[self.driveMode][self.driveSpeed]
-
-    @sea.queuedDashboardEvent
-    def c_changeSpeed(self, button):
-        self.driveSpeed = button.get_text()
-        self.driveGear = self.driveGears[self.driveMode][self.driveSpeed]
-
-    @sea.queuedDashboardEvent
-    def c_stop(self, button):
-        self.superDrive.disable()
-
-    @sea.queuedDashboardEvent
-    def c_compressor(self, button):
-        if self.app is not None:
-            if button.get_text() == "start":
-                self.compressor.start()
-                self.app.compressorGroup.highlight("start")
-            else:
-                self.compressor.stop()
-                self.app.compressorGroup.highlight("stop")
-
 if __name__ == "__main__":
-    wpilib.run(CompetitionBot2020)
+    wpilib.run(RevClientBot)
